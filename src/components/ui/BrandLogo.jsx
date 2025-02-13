51e595ddb186baca8f594e1f2af2fdb9d17a19d5
Dhurba Bhusal <dbhusal@emory.edu>
2025-02-13 11:59:18 -0600

    new logo

--- src/components/ui/BrandLogo.jsx
+++ src/components/ui/BrandLogo.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import brandLogo from "../../assets/logo.jpg"
 
 export default function BrandLogo() {
     return (
-        <div className="flex flex-col">
-            <span className="text-2xl font-eyescript text-black font-bold tracking-wider">
-                Sa<span style={{ color: "#CC963C" }}>ff</span>ron
-            </span>
-            <span className="text-sm text-gray-700">Kitchen & Bar</span>
-        </div>
+        // <div className="flex flex-col">
+        //     <span className="text-2xl font-eyescript text-black font-bold tracking-wider">
+        //         Sa<span style={{ color: "#CC963C" }}>ff</span>ron
+        //     </span>
+        //     <span className="text-sm text-gray-700">Kitchen & Bar</span>
+        // </div>
+        <img src={brandLogo} alt="Saffron Kitchen & Bar" className="h-16" />
     );
 }
