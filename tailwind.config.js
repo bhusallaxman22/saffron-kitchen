d6e07d2e53fcdb558de651218ae4d64ef059f6cb
Dhurba Bhusal <dbhusal@emory.edu>
2025-02-16 12:24:17 -0600

    seo and other changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
       },
       colors: {
         saffron: '#F59E0B',
+        customYellow: '#F0B75E',
       },
       animation: {
         'fade-in': 'fadeIn 0.5s ease-in',
